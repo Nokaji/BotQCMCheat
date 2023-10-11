28ea631f46bc6ec88d57f8850b7c62e0f0dc4e26
Nokaji <nokaji.dev@gmail.com>
2023-10-11 20:19:08 +0200

    update

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs-extra');
 
-const url = 'https://oboulo.lfz.duckdns.org/qcm/qcm_evalv2.php?theme=60&filiere=2&nbQuestion=50';
+const url_base = 'https://oboulo.lfz.duckdns.org/qcm/';
+const url_direct = "qcm_evalv2.php?theme=60&filiere=2&nbQuestion=50";
+const url_auth = 'https://example.com/protected-page';
+const username = 'snt';
+const password = 'snt';
 
-axios.get(url)
+const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+
+let temp_val;
+let temp_num;
+
+axios.get(url_base + url_direct, { headers: { 'Authorization': auth } })
     .then(response => {
         const html = response.data;
         const $ = cheerio.load(html);
         const questions = [];
 
-        $('input#Ref3, input#Answ3').each((i, el) => {
+        $('input').each((i, el) => {
+            temp_val = int();
+            temp_num = int();
             const input = $(el);
             const inputId = input.attr('id');
             const inputVal = input.val().trim();
 
-            if (inputId === 'Ref3') {
+            questions.push({
+                id: inputId,
+                value: inputVal
+            });
+
+            if(inputId.includes('Ref')) {
+                const refNum = inputId.match(/\d+/)[0];
                 question.reference = inputVal;
-                input.val('new_reference_value');
-            } else if (inputId === 'Answ3') {
+                question.refNum = refNum;
+                temp_val = inputVal;
+            }else if (inputId.includes("Answ")){
+                const ansNum = inputId.match(/\d+/)[0];
                 question.answer = inputVal;
-                input.val('new_answer_value');
+                question.ansNum = ansNum;
             }
         });
+
+        url_final = url_base + "controler_notation2.php";
+        url_final += "";
 
         const data = JSON.stringify(questions, null, 2);
 
@@ -30,24 +53,13 @@ axios.get(url)
             if (err) throw err;
             console.log('Questions saved to questions.json');
         });
-    });
 
-const request = require('request');
-const username = 'snt';
-const password = 'snt';
-const url_auth = 'https://example.com/protected-page';
-
-const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
-
-request.get({
-    url: url,
-    headers: {
-        'Authorization': auth
-    }
-}, (error, response, body) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(body);
-    }
-});
+        fs.writeFile("url.txt", url_final, err => {
+            if(err) throw err;
+            console.log('Url saved to url.txt');
+        });
+    })
+    const question = {
+        reference: '',
+        answer: ''
+    };
