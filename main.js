b88699d066667fc41f4775b3e380f02b1349f736
Nokaji <nokaji.dev@gmail.com>
2023-10-12 21:04:25 +0200

    update

--- main.js
+++ main.js
@@ -1,53 +1,47 @@
-
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs-extra');
 
 const url_base = 'https://oboulo.lfz.duckdns.org/qcm/';
 const url_direct = "qcm_evalv2.php?theme=60&filiere=2&nbQuestion=50";
-const url_auth = 'https://example.com/protected-page';
 const username = 'snt';
 const password = 'snt';
 
 const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
 
-let temp_val;
-let temp_num;
+let temp_val, temp_num, url_final, pre_url_json, url_json;
+const questions = {};
 
-axios.get(url_base + url_direct, { headers: { 'Authorization': auth } })
-    .then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-        const questions = [];
+axios.get(url_base + url_direct, { headers: { 'Authorization': auth } }).then(response => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const inputs = [];
 
-        $('input').each((i, el) => {
-            temp_val = int();
-            temp_num = int();
-            const input = $(el);
-            const inputId = input.attr('id');
-            const inputVal = input.val().trim();
+    url_final = url_base + "controler_notation2.php";
 
-            questions.push({
-                id: inputId,
-                value: inputVal
-            });
+    $('input').each((i, el) => {
+        const inputElement = $(el);
+        const inputId = inputElement.attr('id');
+        const inputVal = inputElement.val().trim();
 
-            if(inputId.includes('Ref')) {
-                const refNum = inputId.match(/\d+/)[0];
-                question.reference = inputVal;
-                question.refNum = refNum;
-                temp_val = inputVal;
-            }else if (inputId.includes("Answ")){
-                const ansNum = inputId.match(/\d+/)[0];
-                question.answer = inputVal;
-                question.ansNum = ansNum;
-            }
+        inputs.push({
+            id: inputId,
+            value: inputVal
         });
 
-        url_final = url_base + "controler_notation2.php";
-        url_final += "";
+        if (inputId.includes('Ref')) {
+            const refNum = inputId.match(/\d+/)[0];
+            const ansNum = inputId.replace('Ref', 'Answ');
+            const ansVal = $(`#${ansNum}`).val().trim();
+            questions[inputVal] = ansVal;
+        }
+    });
 
-        const data = JSON.stringify(questions, null, 2);
+    if (Object.keys(questions).length === 0) {
+        console.error('Questions object is empty');
+    } else {
+        const data = JSON.stringify(inputs, null, 2);
+        url_json = encodeURIComponent(JSON.stringify(questions));
 
         fs.writeFile('questions.json', data, err => {
             if (err) throw err;
@@ -55,11 +49,22 @@ axios.get(url_base + url_direct, { headers: { 'Authorization': auth } })
         });
 
         fs.writeFile("url.txt", url_final, err => {
-            if(err) throw err;
+            if (err) throw err;
             console.log('Url saved to url.txt');
         });
-    })
-    const question = {
-        reference: '',
-        answer: ''
-    };
+
+        fs.writeFile("url.json", url_json, err => {
+            if (err) throw err;
+            console.log('Url saved to url.json');
+        });
+
+        // Inclure la deuxième requête GET ici
+        axios.get(url_final + '?jsonReturnString=' + url_json, { headers: { 'Authorization': auth } })
+            .then(secondResponse => {
+                console.log(`Status : ${secondResponse.status}`);
+            })
+            .catch(error => {
+                console.error('Error in the second GET request:', error);
+            });
+    }
+});
